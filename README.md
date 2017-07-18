a429f1d098a4807eb2d2d6a6f685e27815d2431c
Zach Bloomquist <zachary.bloomquist@ge.com>
2017-07-18 16:05:49 -0500

    Enhancements to update set picker

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Make Service-Now Great Again
+# Make ServiceNow Great Again
 
-This Chrome extension contains a variety of tweaks intended to improve the Service-Now user experience.
+This Chrome extension contains a variety of tweaks intended to improve the ServiceNow user experience.
 
 ## Included tweaks
 
@@ -8,4 +8,11 @@ This Chrome extension contains a variety of tweaks intended to improve the Servi
 
 ![QA and Dev favicons](https://i.imgur.com/12Rned0.png)
 
-Service-Now instances with names ending in ```qa``` or ```dev``` will have their favicons (icons displayed in the browser bar) changed to different icons so that Service-Now developers working in multiple environments can easily tell which tab is in which environment.
+ServiceNow instances with names ending in ```qa``` or ```dev``` will have their favicons (icons displayed in the browser bar) changed to different icons so that Service-Now developers working in multiple environments can easily tell which tab is in which environment.
+
+### Update set picker enhancements
+
+![New update set picker](https://i.imgur.com/abhtP8Z.png)
+
+* Click on the stacked paper icon on the right to copy the current update set name to the clipboard.
+* Click on the caret icon on the left to go to the current update set in ServiceNow.
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,12 +2,47 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
+		l('Document loaded, injecting scripts.');
+		try {
+			configureUpdateSetPicker();
+		} catch (e) {
+			l('Could not configure update set picker. ', e)
+		}
 	}
 	}, 10);
 });
+
+function configureUpdateSetPicker() {
+	var pickerIcon = $('.concourse-update-set-picker > div > .icon-document-multiple');
+	pickerIcon.click(function() {
+		var name = $('select#update_set_picker_select > option:selected')[0].label;
+		pickerIcon[0].classList.remove('icon-document-multiple');
+		pickerIcon[0].classList.add('icon-article-document');
+		setTimeout(function() {
+			pickerIcon[0].classList.remove('icon-article-document');
+			pickerIcon[0].classList.add('icon-document-multiple');
+		}, 200);
+		chrome.runtime.sendMessage({
+			'action': 'copy',
+			'value': /^(.*) \[.*\]$/g.exec(name)[1]
+		});
+	});
+	pickerIcon[0].title = 'Copy current update set to clipboard';
+	pickerIcon.before(function () {
+		var e = document.createElement('span');
+		e.classList.add('label-icon', 'icon-console');
+		e.style.marginRight = '10px';
+		e.style.cursor = 'pointer';
+		e.title = 'Go to current update set';
+		e.onclick = function() {
+			window.location.href = '/nav_to.do?uri=/sys_update_set.do?sys_id=' + 
+				$('select#update_set_picker_select > option:selected').val().split(':')[1];
+		}
+		return e;
+	})
+}
+
+function l(message, e) {
+	console.log('[MSGA] ' + message);
+	e ? console.log('[MSGA] Detail: ' + e) : {};
+}
